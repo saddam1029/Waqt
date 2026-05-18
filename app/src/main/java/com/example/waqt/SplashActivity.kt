82037faec8e5a26593d8e41f1336568c9ceeec8f
Syed Saddam <saddam.jaff07@gmail.com>
2026-05-18 22:03:40 +0500

    add splash screen design

--- app/src/main/java/com/example/waqt/SplashActivity.kt
+++ app/src/main/java/com/example/waqt/SplashActivity.kt
@@ -1,6 +1,9 @@
 package com.example.waqt
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,10 @@ class SplashActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 2000)
     }
 }
