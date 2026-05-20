d203488dd08d822cba4a65525f1778927ecf8ccd
Syed Saddam <saddam.jaff07@gmail.com>
2026-05-20 09:56:08 +0500

    fix bottom nav

--- app/src/main/java/com/example/waqt/MainActivity.kt
+++ app/src/main/java/com/example/waqt/MainActivity.kt
@@ -23,7 +23,7 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
-        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
         bottomNav.setupWithNavController(navController)
     }
 }
